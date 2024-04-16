b0f161e5251af3d246703d9be59454c7745e81e0
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-04-16 16:41:07 +0700

    update kafka acls login

--- src/main/java/com/nt/sms_module_worker/config/KafkaConfig.java
+++ src/main/java/com/nt/sms_module_worker/config/KafkaConfig.java
@@ -3,14 +3,21 @@ package com.nt.sms_module_worker.config;
 import java.util.HashMap; 
 import java.util.Map; 
 import org.apache.kafka.clients.consumer.ConsumerConfig; 
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.config.SecurityConfig;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
+import org.apache.kafka.common.security.auth.SecurityProviderCreator;
+import org.apache.kafka.common.security.scram.*;
 import org.apache.kafka.common.serialization.StringDeserializer; 
 import org.springframework.context.annotation.Bean; 
 import org.springframework.context.annotation.Configuration; 
 import org.springframework.kafka.annotation.EnableKafka; 
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory; 
 import org.springframework.kafka.core.ConsumerFactory; 
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory; 
-
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
@@ -18,29 +25,21 @@ import org.springframework.beans.factory.annotation.Value;
 public class KafkaConfig {
     
    
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
+    // @Value("${spring.kafka.bootstrap-servers}")
+    // private String bootstrapServers;
 
-    @Value("${spring.kafka.enable.auto.commit}")
-    private String enableAutoCommitConfig;
+    // @Value("${spring.kafka.enable.auto-commit}")
+    // private String enableAutoCommitConfig;
 
-    @Value("${spring.kafka.consumer.auto-offset-reset}")
-    private String autoOffsetResetConfig;
-
+    // @Value("${spring.kafka.consumer.auto-offset-reset}")
+    // private String autoOffsetResetConfig;
     @Bean
     public ConsumerFactory<String, String> consumerFactory() 
     { 
         Map<String, Object> properties = new HashMap<>();
-        System.out.println("enableAutoCommitConfig:"+ enableAutoCommitConfig);
-        System.out.println("autoOffsetResetConfig:"+ autoOffsetResetConfig);
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        // properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, enableAutoCommitConfig); // close auto commit ack message FALSE
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, autoOffsetResetConfig);
-        properties.put("security.protocol", "SASL_PLAINTEXT");
-        properties.put("sasl.mechanism", "SCRAM-SHA-256");
-        properties.put("sasl.jaas.config", "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"admin\" password=\"admin-secret\";");
         
         
         return new DefaultKafkaConsumerFactory<>(properties);
