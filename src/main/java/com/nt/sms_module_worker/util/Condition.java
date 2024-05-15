7bf7b12adde35e36f6cf442a8d03822e145442af
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-05-15 21:35:41 +0700

    update != all data type

--- src/main/java/com/nt/sms_module_worker/util/Condition.java
+++ src/main/java/com/nt/sms_module_worker/util/Condition.java
@@ -37,7 +37,7 @@ public class Condition {
     public static final boolean doBooleanOperation(String operator,Boolean dataValue, Boolean orConfValue){
         switch (operator) {
             case "!=":
-                return dataValue != orConfValue;
+                return !dataValue.equals(orConfValue);
             case "=":
                 return dataValue.equals(orConfValue);
             default:
@@ -48,7 +48,7 @@ public class Condition {
     public static final boolean doStringOperation(String operator,String dataValue, String orConfValue){
         switch (operator) {
             case "!=":
-                return !(dataValue.equals(orConfValue));
+                return !dataValue.equals(orConfValue);
             case "=":
                 return dataValue.equals(orConfValue);
             case "like":
@@ -80,10 +80,10 @@ public class Condition {
     public static final boolean doNumberOperation(String operator,Integer dataNumber, Integer orConfNumber){
         // int dataNumber = Integer.parseInt(dataValue);
         // int orConfNumber = Integer.parseInt(orConfValue);
-        // System.out.println(operator+" , dataNumber: "+dataNumber+ " , orConfNumber:"+ orConfNumber + " ==> "+(dataNumber == orConfNumber));
+        System.out.println(operator+" , dataNumber: "+dataNumber+ " , orConfNumber:"+ orConfNumber + " ==> "+(!dataNumber.equals(orConfNumber)));
         switch (operator) {
             case "!=":
-                return dataNumber != orConfNumber;
+                return !dataNumber.equals(orConfNumber);
             case "=":
                 return dataNumber.equals(orConfNumber);
             case "<":
