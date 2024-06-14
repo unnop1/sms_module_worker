46bb27be4f7f2f8772328290c923c63fdb9bb2de
คนที่คุณก็รู้ว่าใคร <pawat@computerlogy.com>
2024-06-14 13:20:04 +0700

    test deploy

--- src/main/java/com/nt/sms_module_worker/repo/FixedPhoneRepo.java
+++ src/main/java/com/nt/sms_module_worker/repo/FixedPhoneRepo.java
@@ -10,6 +10,6 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface FixedPhoneRepo extends JpaRepository<FixedPhoneNumberEntity,Long> {
 
-    @Query("SELECT p.PHONENUMBER FROM FIXED_PHONENUMBER p")
+    @Query("SELECT p.phonenumber FROM fixed_phonenumber p")
     List<String> findAllPhoneNumbers();
 }
